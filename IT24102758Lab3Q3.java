6677fe2d607b1626e6e91ea5b647563bac1d2e4d
Priyamalka W D N <it24102758@my.sliit.lk>
2024-11-01 22:36:40 +0530

    Add files via upload

---
+++ IT24102758Lab3Q3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+class IT24102758Lab3Q2 {
+         public static void main(String[] args) {
+             Scanner scanner = new Scanner(System.in);
+
+   
+             System.out.print("Enter the rupee amount: ");
+             int amount = scanner.nextInt();
+
+             int[] denominations = {5000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+
+             int[] count = new int[denominations.length];
+
+             for (int i = 0; i < denominations.length; i++)  {
+                if (amount >= denominations[i]) {  
+                    count[i] = amount / denominations[i];
+                    amount = amount % denominations[i];
+
+
+    }
+}
+
+System.out.println("5000 Notes - " + count[0]);
+System.out.println("1000 Notes - " + count[1]);
+System.out.println("500 Notes - " + count[2]);
+System.out.println("200 Notes - " + count[3]);
+System.out.println("100 Notes - " + count[4]);
+System.out.println("50 Notes - " + count[5]);
+System.out.println("20 Notes - " + count[6]);
+System.out.println("10 Notes - " + count[7]);
+System.out.println("5 Notes - " + count[8]);
+System.out.println("2 Notes - " + count[9]);
+System.out.println("1 Notes - " + count[10]);
+    
+    }
+}
